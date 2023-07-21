a006e7d582622103f6ed2627a42f41ca7ba09035
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-07-21 16:06:45 -0700

    Build RoadmapSelector without logic of passing props in

--- src/components/RoadmapSelector.tsx
+++ src/components/RoadmapSelector.tsx
@@ -1,5 +1,66 @@
+import styled from 'styled-components';
+
 const RoadmapSelector = () => {
-  return <div>RoadmapSelector</div>;
+  const tempList = ['Planned', 'In Progress', 'Live'];
+
+  return (
+    <Wrapper>
+      <Upper>
+        <Title>Roadmap</Title>
+        <ViewButton>
+          <a href=''>View</a>
+        </ViewButton>
+      </Upper>
+      <Lower>
+        {tempList.map(item => (
+          <ItemWrapper>
+            <li key={item}>{item}</li>0
+          </ItemWrapper>
+        ))}
+      </Lower>
+    </Wrapper>
+  );
 };
 
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  background-color: #ffffff;
+  border-radius: 10px;
+  padding: 24px;
+`;
+
+const Upper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Lower = styled.ul`
+  display: flex;
+  flex-direction: column;
+  padding-inline-start: 24px;
+`;
+
+const Title = styled.h2`
+  font-weight: 700;
+  font-size: 18px;
+  letter-spacing: -0.25px;
+`;
+
+const ViewButton = styled.p`
+  font-weight: 600;
+  font-size: 13px;
+  & a {
+    color: #4661e6;
+  }
+  & a:hover {
+    color: #8397f8;
+  }
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 export default RoadmapSelector;
